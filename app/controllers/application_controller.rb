89243a300f3df17daca0a97097260bc67b1fa4f8
Daniel Hernandez <elreymcd@yahoo.com>
2018-08-10 16:02:36 -0500

    set up file structure

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,17 @@
+
+class ApplicationController < Sinatra::Base
+
+  configure do
+    set :public_folder, 'public'
+    set :views, 'app/views'
+    enable :sessions
+    set :session_secret, "elreymcd"
+  end
+
+  get '/' do   
+    binding.pry
+     
+    erb :index
+  end
+  
+end
